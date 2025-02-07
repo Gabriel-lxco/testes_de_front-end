14eaee4600c1f80ed9115234aadac856e9b6371a
3°F <gabriel.laco@aluno.senai.br>
2025-02-07 12:42:50 -0300

    Começando correção atividade aula02

---
+++ aula_02/correcacaoAtt/script.js
@@ -0,0 +1,33 @@
+document.getElementById("btn").addEventListener("click", function(){
+
+
+    //Capturei os elementos HTML
+    const email = document.querySelector("email")
+    const email = document.querySelector("email")
+    const email = document.querySelector("email")
+    const email = document.querySelector("email")
+
+    //Extrair valor digitado pelo usuário no campo de input
+
+    const emailValue = email.value()
+    const emailValue = email.value()
+    const emailValue = email.value()
+    const emailValue = email.value()
+
+    if(emailValue === ""){
+        email.classList.add("erro")
+    }
+
+    if(emailValue === ""){
+        email.classList.add("erro")
+    }
+
+    if(emailValue === ""){
+        email.classList.add("erro")
+    }
+
+    if(emailValue === ""){
+        email.classList.add("erro")
+    }
+
+})
